a0739ec1611350c312b2d8bc2fcfe30999216c19
YoussefGXD <youssef123medhat@gmail.com>
2022-09-24 22:08:02 +0200

    update

--- server.js
+++ server.js
@@ -10,17 +10,35 @@ app.use(bodyPraser.json())
 
 app.post("/register",(req , res)=>{
     const {firstName , lastName} = req.body
-    users.push({firstName , lastName})
+    let id = users.length+1
+    users.push({id , firstName , lastName})
     res.status(200).json({
         massage:"register successfully",
         data: users
     })
 })
+app.patch ("/users/:id", (req , res) => {
+    let id = req.params.id.slice(1)
+    const {firstName , lastName} = req.body
+    users.forEach((x) =>{
+        if (x.id == id ){
+            x.firstName = firstName
+            x.lastName = lastName
+            res.status(200).json({
+                massage: "edit complete",
+                data : x
+            })
+        }
+    })
+    res.status(400).json({
+        massage: "Not found"
+    })
+})
 app.post("/login" , (req, res)=>{
-    const {first , last} = req.body
+    const {firstName , lastName} = req.body
 
     for(let i = 0 ; i < users.length ; i++){
-        if (users[i].firstName === first && users[i].lastName === last){
+        if (users[i].firstName === firstName && users[i].lastName === lastName){
             res.status(200).json({
                 massage:"login successfully"
             })
